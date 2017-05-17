2a39aef748aaf90e82b310ec19ed601e1a849ae7
Andrew Chen <acchen722@gmail.com>
2017-05-16 22:56:16 -0700

    login

--- lamp/cakephp/app/src/Controller/UserController.php
+++ lamp/cakephp/app/src/Controller/UserController.php
@@ -28,36 +28,77 @@ use Cake\View\Exception\MissingTemplateException;
  */
 class UserController extends PagesController
 {
-	public function signup(){	
-   	//print("hello world");  
-    $db = mysqli_connect("localhost", "root", "root", "p3_test"); 
-   
-    //$connection = ConnectionManager::get($this->datasource);
-   
-    if($_SERVER["REQUEST_METHOD"] == "POST")
-      {
-	      $myusername = mysqli_real_escape_string($db,$_POST['email']);
-		    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-			  $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
-				$result = mysqli_query($db,$sql);
-        if($result == false)
-          print("fuck");
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-			  $active = $row['active'];
-			  $count = mysqli_num_rows($result);
-			}
+    public function login()
+    {
+       //print("hello world");
+        $db = mysqli_connect("localhost", "root", "root", "p3_test");
 
-    if($count == 1)
-      {
-	      session_register("myusername");
-	      $_SESSION['login_user'] = $myusername;
-		    header("location: welcome.php");
-		  }
-	  else
-		  {
-				$error = "Your Login Name or Password is invalid";
-      }
+        //$connection = ConnectionManager::get($this->datasource);
 
-     $this->display("profile");
-    }					    
+        if($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+                $email = ($_POST['email']);
+
+                $sql = sprintf("SELECT email FROM Users WHERE email = '%s'", mysqli_real_escape_string($db, $email));  //i change\
+		d id to email
+                $result = mysqli_query($db, $sql);
+
+                $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+                //$active = $row['active'];
+                $count = mysqli_num_rows($result);
+                //$count = count($result);
+                $mypassword = mysqli_real_escape_string($db, $_POST['password']);
+
+                //for debugging purposes
+		/*
+                $query="SELECT * FROM Users";
+                $results = mysqli_query($db, $query);
+                while ($row = mysqli_fetch_array($results)){
+		      echo '<tr>';
+                      foreach($row as $field)
+																							{																			           echo '<td>' . htmlspecialchars($field) . '</td>';
+				 echo "\n";															                        }
+		      echo "\n";
+                }
+		*/
+	        //end of debugging code
+
+                if ($count)
+		{
+			echo 'Username and Password Found';
+		        $this->display("profile");
+                }
+	        else
+                {																				 echo 'Username and Password NOT found: ';														      printf("%s is not in the Database. \n", $email);
+																				             }
+
+        }
+
+
+    }
+
+    public function signup()
+    {
+	$db = mysqli_connect("localhost", "root", "root", "p3_test");
+
+        if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+	        $firstName = ($_POST['firstName']);
+		$lastName = ($_POST['lastName']);
+                $email = ($_POST['email']);
+                //insert the record into the database upon signup
+                $sql = "INSERT INTO Users (firstName, lastName, email) VALUES ('$firstName', '$lastName', '$email')";
+
+                if(mysqli_query($db, $sql))
+	        {
+			echo "new record created successfully";
+			//$this->display("profile");
+		}
+	}
+
+        $this->display("profile");  //change this to display the homepage again bc user has not successfully logged in
+
+    }
 }
+
+
