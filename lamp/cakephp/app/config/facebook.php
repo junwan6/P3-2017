ef32a5815f048bd136cb77ed96cafd11add838af
Andrew Chen <acchen722@gmail.com>
2017-06-06 18:25:25 -0700

    Final dev commit, adding unstaged files

---
+++ lamp/cakephp/app/config/facebook.php
@@ -0,0 +1,15 @@
+<?php
+/**
+  * Get an api_key and secret from facebook and fill in this content.
+  * save the file to app/Config/facebook.php
+  */
+  $config = array(
+  	'Facebook' => array(
+  		'appId' => '1949809171905215',
+  		'apiKey' => 'v2.9',
+  		'secret' => '93155319de119ffcefb0294202e3aacd',
+  		'cookie' => true,
+  		'locale' => 'en_US',
+  		)
+  	);
+?>
